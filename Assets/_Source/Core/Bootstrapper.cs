697e79b47f6c5556a769f639d995f0447c84a479
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-10-23 14:00:50 +0300

    Reinit project

--- Assets/_Source/Core/Bootstrapper.cs
+++ Assets/_Source/Core/Bootstrapper.cs
@@ -1,13 +1,18 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Core
+public class Bootstrapper : MonoBehaviour
 {
-    public class Bootstrapper : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
-        private void Awake()
-        {
-            throw new NotImplementedException();
-        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
