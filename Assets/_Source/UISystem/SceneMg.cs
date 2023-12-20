147f1f28f15d97edc90d1d2e5edf75d2b0019dff
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-12-20 19:38:05 +0300

    added tutorial choice and fixed zoom

--- Assets/_Source/UISystem/SceneMg.cs
+++ Assets/_Source/UISystem/SceneMg.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneMg : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Play()
+    public void Tutorial()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Exit()
