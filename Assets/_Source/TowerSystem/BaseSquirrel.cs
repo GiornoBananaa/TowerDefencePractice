3345204ddbc98151b2ea5913e675b40f8dc7a8ed
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-12-21 23:45:37 +0300

    Added crossbow squirrel model and animations

--- Assets/_Source/TowerSystem/BaseSquirrel.cs
+++ Assets/_Source/TowerSystem/BaseSquirrel.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using DG.Tweening;
-using EnemySystem;
 using UnityEngine;
 
 namespace TowerSystem
@@ -8,6 +7,7 @@ namespace TowerSystem
     public class BaseSquirrel : Tower
     {
         private static readonly int _attackAnimationTriggerHash = Animator.StringToHash("Attack");
+        private static readonly int _attackSpeedAnimationFloatHash = Animator.StringToHash("AttackSpeed");
         [SerializeField] private GameObject _bulletPrefab;
         [SerializeField] private Transform _firePoint;
         protected BulletPool _bulletPool;
@@ -22,7 +22,6 @@ namespace TowerSystem
         {
             if (_bulletPool.TryGetFromPool(out Bullet bullet))
             {
-                Animator.SetTrigger(_attackAnimationTriggerHash);
                 while (!_enemiesInRange[0].gameObject.activeSelf
                        || Vector3.Distance( transform.TransformPoint(_enemyTrigger.center), 
                            _enemiesInRange[0].transform.position) > TowerData.AttackRange * 1.5f)
@@ -35,6 +34,8 @@ namespace TowerSystem
                 {
                     if (_enemiesInRange[0].AddPredictedDamage(bullet.Damage))
                     {
+                        Animator.SetTrigger(_attackAnimationTriggerHash);
+                        Animator.SetFloat(_attackSpeedAnimationFloatHash, Animator.GetCurrentAnimatorStateInfo(0).length/TowerData.AttackCooldown);
                         transform.DOLookAt(_enemiesInRange[0].transform.position, 0.2f,AxisConstraint.Y);
                         bullet.SetTarget(_enemiesInRange[0], TowerData.Attack);
                         bullet.transform.position = _firePoint.position;
